c47f0a1ab720be6b56e352c50cbad5d8e47cc346
Tushar Sharma <tushar.sharma5@publicissapient.com>
2019-11-07 16:52:02 +0530

    interface transaction functions add

--- src/main/java/com/retail/App/repository/TransactionRepository.java
+++ src/main/java/com/retail/App/repository/TransactionRepository.java
@@ -2,7 +2,23 @@ package com.retail.App.repository;
 
 import com.retail.App.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
+@Repository
 public interface TransactionRepository extends JpaRepository <Transaction, Integer> {
 
+
+    @Query("SELECT customer_id, date FROM transaction t where t.amount = :amount ORDER BY date")
+    public List<Transaction> findByAmount(@Param("amount") double amount);
+
+    @Query("SELECT customer_id, date FROM transaction t where t.date = :date ORDER BY date")
+    public List<Transaction> findByDate(@Param("date") Date time);
+
+    @Query("SELECT customer_id, date FROM transaction t where t.product_id = :amount ORDER BY date ")
+    public List<Transaction> findByProductId(int id);
 }
---
+++ src/main/java/com/retail/App/service/TransactionService.java
@@ -0,0 +1,23 @@
+package com.retail.App.service;
+
+import com.retail.App.model.Transaction;
+import com.retail.App.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    private TransactionRepository transactionRepository;
+
+    public List<Transaction> list() {
+        return transactionRepository.findAll();
+    }
+
+    public List<Transaction> findbyAmount(double amount) {
+        return transactionRepository.findByAmount(amount);
+    }
+}
